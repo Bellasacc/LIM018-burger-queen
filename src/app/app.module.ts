c09cdbca88bff71e8af6d3adeff9eaf21f5c1049
Jannery Briceño <jannerymai@gmail.com>
2022-09-15 18:09:55 -0500

    AÃdd nav-bar-header and request-name-client components, and routing from home to request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { RequestNameClientComponent } from './request-name-client/request-name-client.component';
+import { NavBarHeaderComponent } from './nav-bar-header/nav-bar-header.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+
+const routes:Routes = [
+  {
+    path: 'hacer-pedido', 
+    component: RequestNameClientComponent
+  },
+  {
+    path: '', 
+    component: HomeComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    RequestNameClientComponent,
+    NavBarHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/request-name-client/request-name-client.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-name-client',
+  templateUrl: './request-name-client.component.html',
+  styleUrls: ['./request-name-client.component.css']
+})
+export class RequestNameClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
