a0dfa68dca3266517f2754b40a12b1c21717aa97
Bellasacc <bellasacc10@gmail.com>
2022-09-17 20:54:04 -0500

    añadir menu lateral y contenedor de items del menu en  vista de hacer pedido

---
+++ src/app/interfaces/menu.interface.ts
@@ -0,0 +1,5 @@
+export default interface Menu {
+    id?: string;
+    description: string;
+    price: number;
+}
--- src/app/request-name-client/request-name-client.component.ts
+++ src/app/request-name-client/request-name-client.component.ts
@@ -12,4 +12,5 @@ export class RequestNameClientComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
 }
