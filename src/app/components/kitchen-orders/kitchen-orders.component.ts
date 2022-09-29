ab85e9b5f5b0a4377be9ae6234fdc589515e0e1c
Bellasacc <bellasacc10@gmail.com>
2022-09-29 13:06:28 -0500

    añadir la funcion de actualizar el estado de la orden, estilos para el cambio de vista de pendiente a servido #4

--- src/app/components/kitchen-orders/kitchen-orders.component.ts
+++ src/app/components/kitchen-orders/kitchen-orders.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { BurgerService } from 'src/app/services/burger.service';
 
 @Component({
@@ -8,18 +8,46 @@ import { BurgerService } from 'src/app/services/burger.service';
 })
 export class KitchenOrdersComponent implements OnInit {
 
-  constructor(private burgerService: BurgerService) { }
+  // Renderer2 para utilizar el dom de una forma mas segura
+  constructor(private burgerService: BurgerService, private renderer: Renderer2) { }
 
   orders:any[] = [];
+  time: number = 0;
 
-  showStatus(status:string) {
+  @ViewChild('liElements') liElements!: ElementRef;
+  @ViewChild('pending') pending!: ElementRef;
+
+  liElement: string = '';
+
+  showStatus(status: string) {
     this.burgerService.getOrders(status).subscribe(order => {
       this.orders = order;
-    })
+    });
+  }
+
+  async updateOrder(id: string, dateCreation: number) {
+    const dateFinally = new Date().getTime();
+    // falta verificar el tiempo
+    const date = new Date(dateFinally - dateCreation);
+    const time = `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
+    await this.burgerService.changeStatus(id, 'listo', dateFinally, time);
+    // se carga nuevamente los pendientes en la vista
+    this.showStatus('pendiente');
   }
 
   ngOnInit(): void {
     this.showStatus('pendiente');
+  }
+
+  ngAfterViewInit(): void {
+    // el evento listen funciona como un addenventlistener, el primer parametro el elemento del dom para limitar el target
+    // para inicializar el nativeElement usamos el ngAfterViewInit
+    this.renderer.listen(this.liElements.nativeElement, 'click', event => {
+      this.liElement = event.target.textContent;
+      if (this.liElement === 'Servido') {
+        this.renderer.removeClass(this.pending.nativeElement, 'active');
+      }
+    });
   }
 
 }
