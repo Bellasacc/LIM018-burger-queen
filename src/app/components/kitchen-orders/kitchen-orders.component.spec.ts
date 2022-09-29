5fde6bab17ac4ea3de7de4a31d8a30ca533ffbef
Bellasacc <bellasacc10@gmail.com>
2022-09-29 16:24:29 -0500

    añadir providers en los test de kicken-orders #4

--- src/app/components/kitchen-orders/kitchen-orders.component.spec.ts
+++ src/app/components/kitchen-orders/kitchen-orders.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { BurgerService } from 'src/app/services/burger.service';
 
 import { KitchenOrdersComponent } from './kitchen-orders.component';
 
 describe('KitchenOrdersComponent', () => {
   let component: KitchenOrdersComponent;
   let fixture: ComponentFixture<KitchenOrdersComponent>;
-
+  const serviceStub = {
+    getOrders: (status: string) =>
+      of([{ id: '', status: status }]),
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ KitchenOrdersComponent ]
+      declarations: [ KitchenOrdersComponent ],
+      providers: [{ provide: BurgerService, useValue: serviceStub }],
     })
     .compileComponents();
 
