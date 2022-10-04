26d182c7c782eb8e41291575de1645e12d475aa0
Bellasacc <bellasacc10@gmail.com>
2022-10-04 12:07:43 -0500

    añadir test para el componente request-name-client #3

--- src/app/components/view-orders/view-orders.component.spec.ts
+++ src/app/components/view-orders/view-orders.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { BurgerService } from 'src/app/services/burger.service';
 
 import { ViewOrdersComponent } from './view-orders.component';
 
 describe('ViewOrdersComponent', () => {
   let component: ViewOrdersComponent;
   let fixture: ComponentFixture<ViewOrdersComponent>;
-
+  const serviceStub = {
+    getOrders: (status: string) =>
+      of([{ id: '', status: status }]),
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ViewOrdersComponent ]
+      declarations: [ ViewOrdersComponent ],
+      providers: [{ provide: BurgerService, useValue: serviceStub }],
     })
     .compileComponents();
 
