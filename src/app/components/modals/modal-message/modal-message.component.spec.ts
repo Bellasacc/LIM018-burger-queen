16a0cdd2e3f4b6960ff776831ac80005aec82f7b
Bellasacc <bellasacc10@gmail.com>
2022-10-04 18:00:29 -0500

    añadir test en el componente modal-message #3

--- src/app/components/modals/modal-message/modal-message.component.spec.ts
+++ src/app/components/modals/modal-message/modal-message.component.spec.ts
@@ -1,4 +1,5 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { ModalMessageComponent } from './modal-message.component';
 
@@ -20,4 +21,23 @@ describe('ModalMessageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('showModal al ejecutarse, el dialog deberia tener el metodo showModal', fakeAsync(() => {
+    let dialog = fixture.debugElement.query(By.css('dialog')).nativeElement;
+    component.showModal();
+    expect(typeof dialog.showModal).toBe('function');
+  }));
+
+  it('closeModal al ejecutarse, el dialog deberia tener el metodo showClose', fakeAsync(() => {
+    let dialog = fixture.debugElement.query(By.css('dialog')).nativeElement;
+    
+    component.message.content = '¡Envío de pedido exitoso!';
+    fixture.detectChanges();
+    tick();
+
+    expect(fixture.debugElement.query(By.css('p')).nativeElement.textContent).toBe('¡Envío de pedido exitoso!');
+    
+    component.closeModal();
+    expect(typeof dialog.close).toBe('function');
+  }));
 });
